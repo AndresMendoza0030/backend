e943a7d84b23dcc6321ca7ed7a36c2fc930b375b
Andres <00305422@uca.edu.sv>
2024-10-04 05:20:00 -0600

    '2'

--- database/migrations/2024_10_04_111443_create_archivo_table.php
+++ database/migrations/2024_10_04_111443_create_archivo_table.php
@@ -4,22 +4,26 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateArchivoTable extends Migration
+class CreateArchivosTable extends Migration
 {
     public function up()
     {
-        Schema::create('archivo', function (Blueprint $table) {
+        Schema::create('archivos', function (Blueprint $table) {
             $table->id();
             $table->string('nombre'); // Nombre del archivo
             $table->text('contenido')->nullable(); // Contenido del archivo (puede ser una ruta a un archivo en el servidor o contenido embebido)
             $table->foreignId('propietario')->constrained('users')->onDelete('cascade'); // Propietario del archivo
             $table->foreignId('carpeta_id')->constrained('carpeta')->onDelete('cascade'); // Relacionado con la tabla carpeta
+
+            // Relación con la tabla archivo_version para la versión actual
+            $table->foreignId('version_actual_id')->nullable()->constrained('archivo_version')->onDelete('set null');
+
             $table->timestamps(); // created_at y updated_at
         });
     }
 
     public function down()
     {
-        Schema::dropIfExists('archivo');
+        Schema::dropIfExists('archivos');
     }
 }
