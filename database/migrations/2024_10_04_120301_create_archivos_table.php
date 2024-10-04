39beb1d4e596d73e49265e17b563493adede5509
Andres <00305422@uca.edu.sv>
2024-10-04 07:33:02 -0600

    '2'

--- database/migrations/2024_10_04_120301_create_archivos_table.php
+++ database/migrations/2024_10_04_120301_create_archivos_table.php
@@ -8,15 +8,17 @@ class CreateArchivosTable extends Migration
 {
     public function up()
     {
-        Schema::create('archivos', function (Blueprint $table) {
-            $table->id();
-            $table->string('nombre');
-            $table->text('contenido')->nullable();
-            $table->foreignId('propietario')->constrained('users')->onDelete('cascade');
-            $table->foreignId('carpeta_id')->constrained('carpetas')->onDelete('cascade');
-            $table->foreignId('version_actual_id')->nullable()->constrained('archivo_version')->onDelete('set null');
-            $table->timestamps();
-        });
+        if (!Schema::hasTable('archivos')) {
+            Schema::create('archivos', function (Blueprint $table) {
+                $table->id();
+                $table->string('nombre');
+                $table->text('contenido')->nullable();
+                $table->foreignId('propietario')->constrained('users')->onDelete('cascade');
+                $table->foreignId('carpeta_id')->constrained('carpetas')->onDelete('cascade');
+                $table->foreignId('version_actual_id')->nullable()->constrained('archivo_version')->onDelete('set null');
+                $table->timestamps();
+            });
+        }
     }
 
     public function down()
