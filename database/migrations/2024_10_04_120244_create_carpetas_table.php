7423ddf7f5a25597738399b33824c001367d942e
Andres <00305422@uca.edu.sv>
2024-10-04 06:36:52 -0600

    '2'

--- database/migrations/2024_10_04_120244_create_carpetas_table.php
+++ database/migrations/2024_10_04_120244_create_carpetas_table.php
@@ -8,15 +8,17 @@ class CreateCarpetasTable extends Migration
 {
     public function up()
     {
-        Schema::create('carpetas', function (Blueprint $table) {
-            $table->id();
-            $table->string('nombre');
-            $table->foreignId('propietario')->constrained('users')->onDelete('cascade');
-            $table->foreignId('sede_id')->constrained('sedes')->onDelete('cascade');
-            $table->foreignId('unidad_id')->constrained('unidades')->onDelete('cascade');
-            $table->foreignId('id_padre')->nullable()->constrained('carpetas')->onDelete('cascade');
-            $table->timestamps();
-        });
+        if (!Schema::hasTable('carpetas')) {
+            Schema::create('carpetas', function (Blueprint $table) {
+                $table->id();
+                $table->string('nombre');
+                $table->foreignId('propietario')->constrained('users')->onDelete('cascade');
+                $table->foreignId('sede_id')->constrained('sedes')->onDelete('cascade');
+                $table->foreignId('unidad_id')->constrained('unidades')->onDelete('cascade');
+                $table->foreignId('id_padre')->nullable()->constrained('carpetas')->onDelete('cascade');
+                $table->timestamps();
+            });
+        }
     }
 
     public function down()
